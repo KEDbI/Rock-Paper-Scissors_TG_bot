8ea8f245f6d56d311cbea4769c7aba484da62468
Andrew_Inochkin <andrewinochkin@gmail.com>
2024-06-11 16:01:27 +0400

    #added new module 'lexicon_ru.py'

--- database/database.py
+++ database/database.py
@@ -1,1 +1,16 @@
-import sqlite3
+import psycopg2
+from config_data.config import DatabaseConfig, connect_database
+db_config: DatabaseConfig = connect_database()
+try:
+    with psycopg2.connect(database='rock_paper_scissors', user=db_config.db.user, password=db_config.db.password,
+        host=db_config.db.host, port=db_config.db.port) as conn:
+        cursor = conn.cursor()
+        cursor.execute('CREATE TABLE users('
+                           'id int NOT NULL,'
+                           'total_games int,'
+                           'won int,'
+                           'lost int,'
+                           'win_percentage int,'
+                           'PRIMARY KEY(id))')
+except:
+    print("не получилось((")
